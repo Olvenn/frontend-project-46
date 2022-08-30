530838c4db73e7b0be413ef2a35a56bc5cd95e5e
Olvenn <olga-vencel@yandex.ru>
2022-08-30 18:53:34 +0300

    install path

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-const getCompare = (file1, file2) => {
-  return console.log('file');
+import path from 'path';
+import  { readFileSync }  from 'fs';
+
+const makeAbsolutePath = (filepath) => path.isAbsolute(filepath) ? filepath : path.resolve(filepath);
+
+const getFileData = (filepath) => {
+    const file = readFileSync(makeAbsolutePath, 'utf-8');
+    return JSON.parse(file);
+}
+
+const makeCompare = (file1, file2) => {
+  console.log(getFileData(filepath1));
+  console.log(getFileData(filepath2));
 };
+
+export default makeCompare;
