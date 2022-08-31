99e53a00ed76d3a79bb55e3203961bf5a98a8ea0
Olvenn <olga-vencel@yandex.ru>
2022-08-31 14:18:18 +0300

    Fix linter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,12 +6,12 @@ import makeCompare from '../src/index.js';
 const program = new Command();
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1', '-V, --version', 'output the version number')
-    .arguments('<filepath1> <filepath2>')
-    .action((filepath1, filepath2) => {
-      console.log(makeCompare(filepath1, filepath2));
-    })
-    .option('-f, --format <type>', 'output format');
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(makeCompare(filepath1, filepath2));
+  })
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,45 +1,41 @@
 import path from 'path';
-import fs from 'fs';
-import  { readFileSync }  from 'fs';
+import { readFileSync } from 'fs';
 import _ from 'lodash';
 
-//  console.log(getFileData(file1));
-//   console.log(getFileData(file2));
-
-const makeAbsolutePath = (filepath) => path.isAbsolute(filepath) ? filepath : path.resolve(process.cwd(), filepath);
-
+const makePath = (filepath) => {
+  const makeAbsolutePath = (relativePath) => path.resolve(process.cwd(), relativePath);
+  return path.isAbsolute(filepath) ? filepath : makeAbsolutePath(filepath);
+};
 
 const getFileData = (filepath) => {
-    // const absoluteFilePath = path.isAbsolute(filepath) ? filepath : path.resolve(filepath);
-    const file = readFileSync(makeAbsolutePath(filepath), 'utf-8');
-    return JSON.parse(file);
-}
+  const file = readFileSync(makePath(filepath), 'utf-8');
+  return JSON.parse(file);
+};
 
 const makeCompare = (file1, file2) => {
-  //Получаем данные файлов
+  // Получаем данные файлов
   const fileData1 = getFileData(file1);
   const fileData2 = getFileData(file2);
-  //Получаем ключи объектов
+  // Получаем ключи объектов
   const keys1 = Object.keys(fileData1);
   const keys2 = Object.keys(fileData2);
 
   const keys = _.sortBy(_.union(keys1, keys2));
 
   const result = keys
-      .map((key) => {
+    .map((key) => {
       if (fileData1[key] === fileData2[key]) {
         return `  ${key}: ${fileData1[key]}`;
-      };
+      }
       if (!fileData1[key]) {
         return `  + ${key}: ${fileData2[key]}`;
-      };
+      }
       if (!fileData2[key]) {
         return `  - ${key}: ${fileData1[key]}`;
-      };
-      if (fileData1[key] && fileData2[key] && fileData1[key] !== fileData2[key]) {
-        return `  - ${key}: ${fileData1[key]}\n  + ${key}: ${fileData2[key]}`;
-      };
-  });
+      }
+
+      return `  - ${key}: ${fileData1[key]}\n  + ${key}: ${fileData2[key]}`;
+    });
 
   console.log(getFileData(file1));
   console.log(getFileData(file2));
@@ -48,4 +44,3 @@ const makeCompare = (file1, file2) => {
 };
 
 export default makeCompare;
-
