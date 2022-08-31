bb6180962d606f69492f3209f29354ee1b749ead
Olvenn <olga-vencel@yandex.ru>
2022-08-31 11:33:53 +0300

    Change gendiff (keys), chsnge index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,18 +1,17 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import getCompare from '../src/index.js';
+import makeCompare from '../src/index.js';
 
 const program = new Command();
 
 program
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1', '-V, --version', 'output the version number')
-    .argument('<filepath1>')
-    .argument('<filepath2>')
-    .option('-f, --format <type>', 'output format')
-    .action((filepath1, pilepath2) => {
-      getCompare(filepath1, pilepath2);
-    });
+    .arguments('<filepath1> <filepath2>')
+    .action((filepath1, filepath2) => {
+      console.log(makeCompare(filepath1, filepath2));
+    })
+    .option('-f, --format <type>', 'output format');
 
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,16 +1,45 @@
 import path from 'path';
+import fs from 'fs';
 import  { readFileSync }  from 'fs';
+import _ from 'lodash';
 
-const makeAbsolutePath = (filepath) => path.isAbsolute(filepath) ? filepath : path.resolve(filepath);
-
+const makeAbsolutePath = (filepath) => path.isAbsolute(filepath) ? filepath : path.resolve(process.cwd(), filepath);
 const getFileData = (filepath) => {
-    const file = readFileSync(makeAbsolutePath, 'utf-8');
+    const file = readFileSync(makeAbsolutePath(filepath), 'utf-8');
     return JSON.parse(file);
 }
 
 const makeCompare = (file1, file2) => {
-  console.log(getFileData(filepath1));
-  console.log(getFileData(filepath2));
+  //Получаем данные файлов
+  const fileData1 = getFileData(file1);
+  const fileData2 = getFileData(file2);
+  //Получаем ключи объектов
+  const keys1 = Object.keys(fileData1);
+  const keys2 = Object.keys(fileData2);
+
+  const keys = _.sortBy(_.union(keys1, keys2));
+
+  const result = keys
+    .map((key) => {
+      if (fileData1[key] === fileData2[key]) {
+        return `  ${key}: ${fileData1[key]}`;
+      };
+      if (!fileData1[key]) {
+        return `  + ${key}: ${fileData2[key]}`;
+      };
+      if (!fileData2[key]) {
+        return `  - ${key}: ${fileData1[key]}`;
+      };
+      if (fileData1[key] && fileData2[key] && fileData1[key] !== fileData2[key]) {
+        return `  - ${key}: ${fileData1[key]}\n  + ${key}: ${fileData2[key]}`;
+      };
+  });
+
+
+console.log('first file', getFileData(file1));
+console.log('second file', getFileData(file2));
+
+  return result.join('\n');
 };
 
 export default makeCompare;
