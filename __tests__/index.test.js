6eb6bc84b0e1989d4c780e11083700654bda51ba
Olvenn <olga-vencel@yandex.ru>
2022-09-01 16:06:02 +0300

    Add test, jest/global

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,24 @@
-// eslint-disable-next-line no-undef
-test('check diff for two plain json files', () => {
+import { fileURLToPath } from 'url';
+import { dirname, path } from 'path';
+import { test, expect } from '@jest/globals';
+import makeCompare from '../src/index.js';
 
+const result = [
+  '- follow: false',
+  '  host: hexlet.io',
+  '- proxy: 123.234.53.22',
+  '- timeout: 50',
+  '+ timeout: 20',
+  '+ verbose: true',
+];
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('check makeCompare with json files', () => {
+  const filePath1 = getFixturePath('file1.json');
+  const filePath2 = getFixturePath('file2.json');
+  expect(typeof makeCompare(filePath1, filePath2)).toBe('string');
 });
