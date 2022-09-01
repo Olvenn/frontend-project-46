5e2a0534f3e15efb176aab31bc4122ea42b9ccb7
Olvenn <olga-vencel@yandex.ru>
2022-09-01 17:34:33 +0300

    Task-4. Finish task. Add test for makeCompare file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,24 +1,25 @@
 import { fileURLToPath } from 'url';
-import { dirname, path } from 'path';
+import path from 'path';
 import { test, expect } from '@jest/globals';
 import makeCompare from '../src/index.js';
 
 const result = [
-  '- follow: false',
+  '  + follow: undefined',
   '  host: hexlet.io',
-  '- proxy: 123.234.53.22',
-  '- timeout: 50',
-  '+ timeout: 20',
-  '+ verbose: true',
+  '  - proxy: 123.234.53.22',
+  '  - timeout: 50\n  + timeout: 20',
+  '  + verbose: true',
 ];
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
+const filePath1 = getFixturePath('file1.json');
+const filePath2 = getFixturePath('file2.json');
+
 test('check makeCompare with json files', () => {
-  const filePath1 = getFixturePath('file1.json');
-  const filePath2 = getFixturePath('file2.json');
+  expect(makeCompare(filePath1, filePath2)).toEqual(result.join('\n'));
   expect(typeof makeCompare(filePath1, filePath2)).toBe('string');
 });
