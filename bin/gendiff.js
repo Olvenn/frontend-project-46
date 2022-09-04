0d515ec3deefbf62692a1666f46b6d6ab422739a
Olvenn <olga-vencel@yandex.ru>
2022-09-04 13:35:25 +0300

    Task-7. Add 3 option in commander file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,10 +9,10 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(makeCompare(filepath1, filepath2));
-  })
   // Добавьте текущий форматер 'stylish' как форматер по умолчанию для библиотеки.
-  .option('-f, --format <type>', 'output format', 'stylish');
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2) => {
+    console.log(makeCompare(filepath1, filepath2, program.opts().format));
+  });
 
 program.parse();
