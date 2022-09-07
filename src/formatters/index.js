3a48c1a9dec1454eb77759d3b6b010a4ab1ff40e
Olvenn <olga-vencel@yandex.ru>
2022-09-07 12:53:40 +0300

    Fix linter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -17,24 +17,3 @@ const getResult = (type) => {
 };
 
 export default getResult;
-
-// import makeStylishResult from './stylish.js';
-// import makePlainResult from './plain.js';
-
-// const makeJsonResult = (diffTree) => JSON.stringify(diffTree);
-
-// const getResult = (data, typeFormat) => {
-//   switch (typeFormat) {
-//     case 'stylish':
-//       return makeStylishResult(data);
-//     case 'plain':
-//       return makePlainResult(data);
-//     case 'json':
-//       return makeJsonResult(data);
-//     default:
-//       throw new Error('Unknown file type.');
-//   }
-// };
-
-// export default getResult;
-
